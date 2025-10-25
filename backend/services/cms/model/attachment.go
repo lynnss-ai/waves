9c4a9e15a21243f47322007b150ed0e0b3fffdfd
lynnss <lynnss.ai@hotmail.com>
2025-10-25 15:44:08 +0800

    refactor(model): 将附件相关模型从admin服务迁移至cms服务
    迁移附件分类模型和附件模型到cms服务，并增强附件模型的文件类型枚举定义

--- backend/services/cms/model/attachment.go
+++ backend/services/cms/model/attachment.go
@@ -8,16 +8,24 @@ package model
 
 import "gorm.io/gorm"
 
+const (
+	FileTypeEnumImage FileTypeEnum = iota + 1 // 图片
+	FileTypeEnumVideo                         // 视频
+	FileTypeEnumAudio                         // 音频
+	FileTypeEnumFile                          // 文件
+)
+
 type (
 	AttachmentModel interface{}
 
-	Attachment struct {
+	FileTypeEnum int
+	Attachment   struct {
 		ID                   string          `json:"id" gorm:"column:id;primaryKey;type:varchar(32)"`                                       // ID
 		AttachmentCategoryID string          `json:"attachment_category_id" gorm:"column:attachment_category_id;type:varchar(32);not null"` // 分类ID
 		FileName             string          `json:"file_name" gorm:"column:file_name;type:varchar(255);not null"`                          // 文件名
 		FileSize             int64           `json:"file_size" gorm:"column:file_size;type:bigint"`                                         // 文件大小
 		FileURL              string          `json:"file_url" gorm:"column:file_url;type:varchar(255);not null"`                            // 文件URL
-		FileType             string          `json:"file_type" gorm:"column:file_type;type:varchar(64)"`                                    // 文件类型
+		FileType             FileTypeEnum    `json:"file_type" gorm:"column:file_type;type:int;not null"`                                   // 文件类型
 		CreatedAt            int64           `json:"created_at" gorm:"autoCreateTime:milli"`                                                // 创建时间
 		UpdatedAt            int64           `json:"updated_at" gorm:"autoUpdateTime:milli"`                                                // 更新时间
 		DeletedAt            *gorm.DeletedAt `json:"deleted_at"`                                                                            // 删除时间
